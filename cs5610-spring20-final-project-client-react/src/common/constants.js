4dacb9cb54fe16c6192442b33cae549889e1f027
xiaojinhe <irisxjhe@gmail.com>
2020-03-04 15:08:51 -0800

    added reviews api

--- cs5610-spring20-final-project-client-react/src/common/constants.js
+++ cs5610-spring20-final-project-client-react/src/common/constants.js
@@ -1,5 +1,11 @@
-const API_KEY = "dc779fdd680071c77d81b911adeed900";
+const TMDB_API_KEY = "dc779fdd680071c77d81b911adeed900";
+const NYT_API_KEY = "kAsxh51CHkFdmA0YV8ifKEJwEvbF0Ux9";
+
 export const MOVIE_DETAIL_API_URL =
-  (movieId) => `https://api.themoviedb.org/3/movie/${movieId}?api_key=${API_KEY}`;
+  (movieId) => `https://api.themoviedb.org/3/movie/${movieId}?api_key=${TMDB_API_KEY}`;
+export const MOVIE_REVIEWS_APT_URL =
+  (movieTitle) => `https://api.nytimes.com/svc/movies/v2/reviews/search.json?query=${movieTitle}&api-key=${NYT_API_KEY}`;
 
 export const FIND_MOVIE_BY_ID = "FIND_MOVIE_BY_ID";
+export const FIND_REVIEWS_FOR_MOVIE = "FIND_REVIEWS_FOR_MOVIE";
+export const FIND_ALL_MOVIE_INFO_BY_ID = "FIND_ALL_MOVIE_INFO_BY_ID";
