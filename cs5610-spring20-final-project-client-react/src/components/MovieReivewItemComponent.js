f30c8dc8989ed16f66f61c59f7d59b8d3e3d96b9
xiaojinhe <irisxjhe@gmail.com>
2020-03-05 14:22:42 -0800

    refactored the movie review list component for re-use in movie detail

--- cs5610-spring20-final-project-client-react/src/components/MovieReivewItemComponent.js
+++ cs5610-spring20-final-project-client-react/src/components/MovieReivewItemComponent.js
@@ -7,16 +7,16 @@ const MovieReviewItemComponent = ({review}) =>
          <img className="img-thumbnail" src={review.multimedia.src}/>
        </div>
       }
-        <div className="col-10">
-            <div>
-                {/*//todo: change a to the link to */}
-                <a href={review.link.url}
-                   className="font-weight-bold">{review.headline}</a>
-            </div>
-            <small>Review for {review.display_title}</small>
-            <div>Published at {review.publication_date}</div>
-            <div>{review.summary_short}</div>
+      <div className="col-10">
+        <div>
+          {/*//todo: change a to the link to */}
+          <a href={review.link.url}
+             className="font-weight-bold">{review.headline}</a>
         </div>
+        <small>Review for {review.display_title}</small>
+        <div>Published at {review.publication_date}</div>
+        <div>{review.summary_short}</div>
+      </div>
     </div>;
 
 export default MovieReviewItemComponent
