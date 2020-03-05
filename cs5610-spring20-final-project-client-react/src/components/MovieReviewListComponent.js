6022f6eeca9138c749fcdd613b221d027cea03af
Jasmine Li <42844353+Hazel69@users.noreply.github.com>
2020-03-05 14:41:15 -0800

    Revert "Dev/add components to movie detail page"

--- cs5610-spring20-final-project-client-react/src/components/MovieReviewListComponent.js
+++ cs5610-spring20-final-project-client-react/src/components/MovieReviewListComponent.js
@@ -1,10 +1,26 @@
 import React from "react";
+import MovieReviewService from "../services/MovieReviewService";
 import MovieReviewItemComponent from "./MovieReivewItemComponent";
 
 class MovieReviewListComponent extends React.Component {
+
+    state = {
+        pickedReviews: []
+    };
+
+    componentDidMount() {
+        MovieReviewService.findCriticPickedMovieReviews()
+            .then(response => {
+                console.log(response);
+                this.setState({
+                    pickedReviews: response.results.slice(0, this.props.displayNum)
+                })
+            })
+    }
+
     render() {
         return (
-            this.props.pickedReviews && this.props.pickedReviews.map((review, index) =>
+            this.state.pickedReviews.map((review, index) =>
                 <MovieReviewItemComponent review={review}
                                           key={index}/>)
         )
