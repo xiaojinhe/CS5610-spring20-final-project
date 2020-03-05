a4d15c889a817f76e49c0d8380c3758ec3a3b4d8
Hazel69 <lsm0609kk@gmail.com>
2020-03-05 11:18:15 -0800

    change link url

--- cs5610-spring20-final-project-client-react/src/components/SearchResultComponents/MovieItemComponent.js
+++ cs5610-spring20-final-project-client-react/src/components/SearchResultComponents/MovieItemComponent.js
@@ -7,14 +7,14 @@ const MovieItemComponent = ({movie}) =>
     <div className="row p-2">
         <div className="col-2">
 
-            <Link to={`/movie/${movie.id}`}>
+            <Link to={`/details/${movie.id}`}>
                 <img src={TMDB_IMAGE_URL(200, movie.poster_path)}
                      alt="Poster"
                      className="img-thumbnail"/>
             </Link>
         </div>
         <div className="col-10">
-            <Link to={`/movie/${movie.id}`} className="lead">{movie.title}</Link>
+            <Link to={`/details/${movie.id}`} className="lead">{movie.title}</Link>
             <div><RatingStar rating={movie.vote_average}/></div>
             <div>Release date: {movie.release_date}</div>
             <p>{movie.overview}</p>
