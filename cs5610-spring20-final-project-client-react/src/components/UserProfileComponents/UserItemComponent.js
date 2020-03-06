19e188d129caccb416d0a338594c044aaa2d5ce4
lyixu <xu.liu@husky.neu.edu>
2020-03-06 10:33:18 -0800

    change user item component

--- cs5610-spring20-final-project-client-react/src/components/UserProfileComponents/UserItemComponent.js
+++ cs5610-spring20-final-project-client-react/src/components/UserProfileComponents/UserItemComponent.js
@@ -1,9 +1,19 @@
 import React from "react";
+import {USER_ICON_PATH} from "../../common/constants";
 
 const UserItemComponent = ({user}) =>
-  <div className="row mt-1 px-3">
-    <a href={`/profile/${user.uid}`}
-       className="font-weight-bold">{user.username}</a>
+  <div className="row mt-1 py-2 px-2">
+    <div className="col-2">
+      <img src={USER_ICON_PATH}
+           className="rounded-circle mx-auto d-flex border p-3"
+           height="80"
+           width="80"
+           alt="user icon"/>
+    </div>
+    <div className="col-10 pt-3">
+      <a href={`/profile/${user.uid}`}
+         className="font-weight-bold">{user.username}</a>
+    </div>
   </div>
 
 
