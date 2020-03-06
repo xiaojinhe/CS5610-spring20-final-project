a0d58722e42d742ce53c71fec90422111e495e45
lyixu <xu.liu@husky.neu.edu>
2020-03-06 13:16:29 -0800

    adjust user item component style

--- cs5610-spring20-final-project-client-react/src/components/UserProfileComponents/UserItemComponent.js
+++ cs5610-spring20-final-project-client-react/src/components/UserProfileComponents/UserItemComponent.js
@@ -2,15 +2,15 @@ import React from "react";
 import {USER_ICON_PATH} from "../../common/constants";
 
 const UserItemComponent = ({user}) =>
-  <div className="row mt-1 py-2 px-2">
-    <div className="col-2">
+  <div className="row mt-1 py-2">
+    <div className="col-1">
       <img src={USER_ICON_PATH}
-           className="rounded-circle mx-auto d-flex border p-3"
+           className="rounded-circle mx-1 d-flex border p-3"
            height="80"
            width="80"
            alt="user icon"/>
     </div>
-    <div className="col-10 pt-3">
+    <div className="col-11 pt-2">
       <a href={`/profile/${user.uid}`}
          className="font-weight-bold">{user.username}</a>
     </div>
