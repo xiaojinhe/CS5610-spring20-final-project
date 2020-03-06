92d71c4e21975657a63185104b6fc345bf366f49
xiaojinhe <irisxjhe@gmail.com>
2020-03-06 14:33:05 -0800

    enable movie card image routing and text-truncate for long name

--- cs5610-spring20-final-project-client-react/src/components/MovieCardComponent.js
+++ cs5610-spring20-final-project-client-react/src/components/MovieCardComponent.js
@@ -5,17 +5,18 @@ import {Link} from "react-router-dom";
 const MovieCardComponent = ({movie}) =>
     <div className="col-4 col-sm-3 col-md-2">
         <div className="card">
+          <Link to={`/details/${movie.id}`}>
             <img className="card-img-top" src={TMDB_IMAGE_URL(500, movie.poster_path)}
                  alt="Preview"/>
+          </Link>
         </div>
         <div className="card-body p-1">
-            <div className="card-title">
+            <div className="card-title text-truncate">
                 <Link to={`/details/${movie.id}`}>
                     {movie.title}
                 </Link>
-                <span className="ml-2">{movie.vote_average}</span>
+                <span className="ml-2 text-right">{movie.vote_average}</span>
             </div>
-
         </div>
     </div>;
 
