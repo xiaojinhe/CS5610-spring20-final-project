53dcee9f42993ef8f7f28aeacaa0a34a976456f8
lyixu <xu.liu@husky.neu.edu>
2020-03-25 00:06:14 -0700

    added server.js

---
+++ cs5610-spring20-final-project-server-node/server.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+
+var app = express();
+
+// For parsing application/json
+app.use(bodyParser.json());
+
+// Configure CORS
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", '*');
+    res.header("Access-Control-Allow-Headers",
+               "Content-Type, X-Requested-With, Origin");
+    res.header("Access-Control-Allow-Methods",
+               "GET, POST, PUT, PATCH, DELETE, OPTIONS");
+    next();
+});
+
+require('data/db')
+require('controllers/user.controller.server')(app)
+
+app.listen(process.env.PORT || 3000);
