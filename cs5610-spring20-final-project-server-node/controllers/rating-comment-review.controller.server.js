082e7e2e12f81f09f575fc7bb26a17c266a597ce
lyixu <xu.liu@husky.neu.edu>
2020-03-26 13:29:37 -0700

    update review/comment controller

--- cs5610-spring20-final-project-server-node/controllers/rating-comment-review.controller.server.js
+++ cs5610-spring20-final-project-server-node/controllers/rating-comment-review.controller.server.js
@@ -1,7 +1,14 @@
-const userDao = require('../data/models/user.dao.server');
-const RCRDao = require('../data/models/rating-comment-review.dao.server');
+const userDao = require('../data/daos/user.dao.server');
+const RCRDao = require('../data/daos/rating-comment-review.dao.server');
 
 module.exports = function (app) {
+
+    // get all
+    app.get('/api/posts', (req, res) => {
+        RCRDao.findAllRatingAndCommentOrReviews()
+            .then(posts => res.json(posts))
+    });
+
     /* ========= REVIEWS ======== */
     // get reviews of a movie
     app.get('/api/movies/:mid/reviews', (req, res) => {
@@ -15,11 +22,15 @@ module.exports = function (app) {
         const tmdbId = req.params['mid'];
         RCRDao.createRatingAndCommentOrReview(req.body)
             .then(review => {
-                // TODO: move code to DAO?
-                userDao.updateUserRatingAndCommentOrReview(review.userId, review._id)
-                res.json(review)
+                if (review) {
+                    userDao.updateUserRatingAndCommentOrReview(review.userId, review._id)
+                    res.json(review)
+                }
             })
     });
+
+    // TODO: DELETE a review
+    // TODO: like/dislike a review
 
     /* ========= COMMENTS ======== */
     // get comments of a movie
@@ -28,4 +39,18 @@ module.exports = function (app) {
         RCRDao.findAllRatingAndCommentOrReviewsForMovie(tmdbId)
             .then(comments => res.json(comments))
     });
+
+    // create a new comment
+    app.post('/api/movies/:mid/comments', (req, res) => {
+        const tmdbId = req.params['mid'];
+        RCRDao.createRatingAndCommentOrReview(req.body)
+            .then(comment => {
+                if (comment) {
+                    userDao.updateUserRatingAndCommentOrReview(comment.userId, comment._id)
+                    res.json(comment)
+                }
+            })
+    });
+
+    // TODO: DELETE a comment
 }
