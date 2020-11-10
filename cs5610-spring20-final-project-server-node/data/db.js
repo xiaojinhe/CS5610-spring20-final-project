d0b540253379e8793c47ea8253626f36c0f0f5e6
lyixu <xu.liu@husky.neu.edu>
2020-11-09 16:09:42 -0800

    update db connection string for atlas

--- cs5610-spring20-final-project-server-node/data/db.js
+++ cs5610-spring20-final-project-server-node/data/db.js
@@ -2,8 +2,8 @@ module.exports = function() {
   const mongoose = require('mongoose');
   const databaseName = 'movie-time';
   let connectionString = 'mongodb://localhost:27017/' + databaseName;
-  if (process.env.MONGODB_URI) {
-    connectionString = process.env.MONGODB_URI
+  if (process.env.MONGODB_CONNECTION) {
+    connectionString = process.env.MONGODB_CONNECTION
   }
   console.log('connectionString:', connectionString);
   mongoose.connect(connectionString, {
